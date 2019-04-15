3940c025c44e83ab4ac653182d582a045a17b45c
fpi <git@pie.tf>
2019-04-15 12:05:27 +0200

    Rename image names in dockerbuild and push scripts

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-docker build shop-db2/          -t fpiper/shop-db2
-docker build shop-db2-admin/    -t fpiper/shop-db2-admin
-docker build shop-db2-frontend/ -t fpiper/shop-db2-frontend
+git checkout x86
+docker build shop-db2/          -t fpiper/shop-db2:x86
+docker build shop-db2-admin/    -t fpiper/shop-db2-admin:x86
+docker build shop-db2-frontend/ -t fpiper/shop-db2-frontend:x86
+
+git checkout arm
+docker build shop-db2/          -t fpiper/shop-db2:arm
+docker build shop-db2-admin/    -t fpiper/shop-db2-admin:arm
+docker build shop-db2-frontend/ -t fpiper/shop-db2-frontend:arm
--- dockerpush.sh
+++ dockerpush.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker push fpiper/shop-db2
-docker push fpiper/shop-db2-admin
-docker push fpiper/shop-db2-frontend
+docker push fpiper/shop-db2:x86
+docker push fpiper/shop-db2-admin:x86
+docker push fpiper/shop-db2-frontend:x86
+
+docker push fpiper/shop-db2:arm
+docker push fpiper/shop-db2-admin:arm
+docker push fpiper/shop-db2-frontend:arm
